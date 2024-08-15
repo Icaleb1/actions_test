6823ed54d88f624c05d2c88ec47f2eda8dbb15aa
Icaleb1 <calebfernandes080@gmail.com>
2024-08-15 11:03:01 -0300

    commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,22 +14,22 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
- /*  it('should create the app', () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  /* it(`should have as title 'senac-frontend-202x'`, () => {
+  it(`should have as title 'senac-frontend-202x'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('senac-frontend-202x');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('senac-frontend-202x app is running!');
-  }); */ 
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('.content span')?.textContent).toContain('senac-frontend-202x app is running!');
+  // });
 });
